52d8edace9b5bf757156c00718fac9cf03bfa00c
unknown <arkadiusz.rajski1@gmail.com>
2021-11-09 21:28:22 +0100

    Initial commit - Base application scaffold

---
+++ BikeApp/BikeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BikeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BikeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ BikeApp/BikeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BikeApp.Models;
+using BikeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BikeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
