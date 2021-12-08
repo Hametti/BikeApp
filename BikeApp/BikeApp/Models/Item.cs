1987cb0146fd3d6d9b3db29c04511a08b174bf4d
PowerfulMind128 <71188156+PowerfulMind128@users.noreply.github.com>
2021-12-08 15:49:45 +0100

    "Your Routes" now operates with an SQLite database

--- BikeApp/BikeApp/Models/Item.cs
+++ BikeApp/BikeApp/Models/Item.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using SQLite;
+using System;
+
 
 namespace BikeApp.Models
 {
     public class Item
     {
+        [PrimaryKey]
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
